a505ccf61a2b315252662486f55076122a86ec21
Nicklas H. <gta5.r6s.bf4@gmail.com>
2024-06-06 14:51:54 +0200

    Add AD users full name to JWT token

--- back-end/lib/_auth.py
+++ back-end/lib/_auth.py
@@ -13,6 +13,7 @@ from logging import Logger, DEBUG, INFO
 from pydantic import BaseModel
 
 from lib._logger import LogHelper
+from lib._utils import get_full_name_from_ldap
 
 SECRET_KEY = "fcd67c9b07b2d022a3cff8570a1f48b0e73d78abefe3156aa6fde53afacf0210"  # TODO: CHANGE BEFORE DEPLOYMENT AND MOVE TO ENVIRONMENT VARIABLES
 ALGORITHM = "HS256"
@@ -46,7 +47,7 @@ async def authenticate_user(
     form_data: OAuth2PasswordRequestForm = Depends(),
 ) -> dict[str, str]:
     try:
-        await authenticate_user_ldap(
+        conn = await authenticate_user_ldap(
             username=form_data.username, password=form_data.password
         )
     except LDAPInvalidCredentialsResult:
@@ -62,9 +63,13 @@ async def authenticate_user(
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    full_name: str = await get_full_name_from_ldap(
+        connection=conn, username=form_data.username
+    )
     encoded_jwt: str = await create_access_token(
         data={
             "sub": form_data.username,
+            "full_name": full_name,
         },
         expires_delta=access_token_expires,
     )
---
+++ back-end/lib/_utils.py
@@ -0,0 +1,43 @@
+from ldap3 import Connection, ALL_ATTRIBUTES  # type: ignore
+from typing import List
+
+
+async def get_full_name_from_ldap(connection: Connection, username: str) -> str:
+    """
+    Retrieves the full name of a user from LDAP based on their username.
+
+    Args:
+        connection (Connection): The LDAP connection object.
+        username (str): The username of the user.
+
+    Returns:
+        str: The full name of the user.
+
+    Raises:
+        RuntimeError: If the LDAP search fails.
+        ValueError: If no matching entries are found or if none of the expected attributes are found.
+    """
+    try:
+        connection.search(
+            search_base="ou=next,dc=next,dc=dev",
+            search_filter=f"(sAMAccountName={username})",
+            attributes=[ALL_ATTRIBUTES],
+        )
+    except Exception as e:
+        raise RuntimeError("LDAP search failed") from e
+
+    if len(connection.entries) == 0:
+        raise ValueError("No matching entries found")
+
+    attributes: List[str] = ["displayName", "cn", "name", "distinguishedName"]
+    for attr in attributes:
+        if attr in connection.entries[0]:
+            if attr == "distinguishedName":
+                fields: List[str] = connection.entries[0][attr].value.split(",")
+                for field in fields:
+                    if field.startswith("CN="):
+                        return field.split("=")[1]
+            else:
+                return connection.entries[0][attr].value
+
+    raise ValueError("None of the expected attributes were found")
