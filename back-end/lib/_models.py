91dc1537b8a771ce80f6b095eef7fc31441ae82d
Nicklas H. <gta5.r6s.bf4@gmail.com>
2024-06-13 12:46:46 +0200

    Added model for questionnaire test endpoint

---
+++ back-end/lib/_models.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Option(BaseModel):
+    value: int
+    label: str
+
+
+class Question(BaseModel):
+    id: int
+    text: str
+    options: List[Option]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "question": "What is your favorite programming language?",
+                "options": [
+                    {"option": 1, "text": "Python"},
+                    {"option": 2, "text": "JavaScript"},
+                    {"option": 3, "text": "Java"},
+                ],
+            }
+        }
