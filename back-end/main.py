c82238445b3287c02436ccb246a48cef82afbe26
Nicklas H. <gta5.r6s.bf4@gmail.com>
2024-06-14 09:44:23 +0200

    Massively improved docstrings for endpoints, improving documentation both programmer-side and API-side for auto-docs

--- back-end/main.py
+++ back-end/main.py
@@ -20,6 +20,8 @@ from lib._auth import (
 from lib._questions import questionnaire
 from lib._models import Question, AllQuestions
 
+# noqa: W291 (trailing whitespace) prevents Flake8 from complaining about trailing whitespace. Used for docstrings.
+# fmt: off/on (black formatting) disables/enables Black formatting for the code block. Used for docstrings.
 
 logger: Logger = LogHelper.create_logger(
     logger_name="backend API (main)",
@@ -90,41 +92,75 @@ async def read_root() -> RedirectResponse:
 async def read_protected(
     token_data: TokenData = Depends(dependency=get_token_data),
 ) -> dict[str, str]:
+    # fmt: off
     """
     Test endpoint to check if the token is valid and to return the token data.
-    """
+
+    username: NH, MF, AMT, userteacher or sysadmin  
+    password: Pa$$w0rd
+
+    - **arg**:  `/api/v1/protected`
+
+    - **return**:  `{"username": str, "full_name": str, "scope": str, "uuid": str}`: The token data.
+
+    - **raises**:  `HTTPException`: A 401 error if the token is invalid
+    """  # noqa: W291
+    # fmt: on
     return {**token_data.model_dump()}
 
 
 @app.get(
-    path="/protected/elev",
+    path="/protected/student",
     tags=["users"],
     response_model=TokenData,
     response_description="The token data",
 )
-async def read_protected_elev(
+async def read_protected_student(
     token_data: TokenData = Depends(dependency=get_token_data),
-    is_elev=Depends(dependency=is_student),
+    is_student=Depends(dependency=is_student),
 ) -> dict[str, str]:
+    # fmt: off
     """
     Test endpoint to check if the token is valid, if the user role matches the required role, and to return the token data.
-    """
+
+    username: NH, MF or AMT  
+    password: Pa$$w0rd
+
+    - **arg**:  `/api/v1/protected/student`
+
+    - **return**:  `{"username": str, "full_name": str, "scope": str, "uuid": str}`: The token data.
+
+    - **raises**:  `HTTPException`: A 401 error if the token is invalid or a 403 error if the user does not have the required permissions
+    """  # noqa: W291
+    # fmt: on
+
     return {**token_data.model_dump()}
 
 
 @app.get(
-    path="/protected/laerer",
+    path="/protected/teacher",
     tags=["users"],
     response_model=TokenData,
     response_description="The token data",
 )
-async def read_protected_laerer(
+async def read_protected_teacher(
     token_data: TokenData = Depends(dependency=get_token_data),
-    is_laerer=Depends(dependency=is_teacher),
+    is_teacher=Depends(dependency=is_teacher),
 ) -> dict[str, str]:
+    # fmt: off
     """
     Test endpoint to check if the token is valid, if the user role matches the required role, and to return the token data.
-    """
+
+    username: userteacher  
+    password: Pa$$w0rd
+
+    - **arg**:  `/api/v1/protected/teacher`
+
+    - **return**:  `{"username": str, "full_name": str, "scope": str, "uuid": str}`: The token data.
+
+    - **raises**:  `HTTPException`: A 401 error if the token is invalid or a 403 error if the user does not have the required permissions
+    """  # noqa: W291
+    # fmt: on
     return {**token_data.model_dump()}
 
 
@@ -138,9 +174,20 @@ async def read_protected_admin(
     token_data: TokenData = Depends(dependency=get_token_data),
     is_admin=Depends(dependency=is_admin),
 ) -> dict[str, str]:
+    # fmt: off
     """
     Test endpoint to check if the token is valid, if the user role matches the required role, and to return the token data.
-    """
+
+    username: sysadmin  
+    password: Pa$$w0rd
+
+    - **arg**:  `/api/v1/protected/admin`
+
+    - **return**:  `{"username": str, "full_name": str, "scope": str, "uuid": str}`: The token data.
+
+    - **raises**:  `HTTPException`: A 401 error if the token is invalid or a 403 error if the user does not have the required permissions
+    """  # noqa: W291
+    # fmt: on
     return {**token_data.model_dump()}
 
 
@@ -151,6 +198,15 @@ async def read_protected_admin(
 async def read_question(
     id: int,
 ) -> Dict[str, Union[str, List[Dict[str, Union[int, str]]]]]:
+    """
+    Call this endpoint with the ID of the question you want to retrieve.
+
+    - **arg**:  `/api/v1/questions/?id={id}` (int): The ID of the question to retrieve.
+
+    - **return**:  `{"id": int, "text": str, "options": [{"value": int, "label": str}]}`: The question with the specified ID.
+
+    - **raises**:  `HTTPException`: A 404 error if the question is not found.
+    """
     for question in questionnaire["questions"]:
         if question["id"] == str(object=id):
             return question
@@ -167,4 +223,18 @@ async def read_question(
 async def read_questions() -> (
     Dict[str, List[Dict[str, Union[str, List[Dict[str, Union[int, str]]]]]]]
 ):
-    return questionnaire
+    """
+    Call this endpoint to retrieve all questions.
+
+    - **arg**:  `/api/v1/questions/all`
+
+    - **return**:  `{"questions": [{"id": int, "text": str, "options": [{"value": int, "label": str}]}]}`: All questions.
+
+    - **raises**:  `HTTPException`: A 404 error if no questions are found.
+    """
+    if len(questionnaire["questions"]) > 0:
+        return questionnaire
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="No questions found",
+    )
