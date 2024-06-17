5b5b3bc49c984228d98b52b48b0b59c29392c517
Nicklas H. <gta5.r6s.bf4@gmail.com>
2024-06-17 14:16:27 +0200

    Update auth constants to retrieve from new settings

--- backend/lib/auth/constants.py
+++ backend/lib/auth/constants.py
@@ -1,9 +1,15 @@
-SECRET_KEY = "fcd67c9b07b2d022a3cff8570a1f48b0e73d78abefe3156aa6fde53afacf0210"  # TODO: CHANGE BEFORE DEPLOYMENT AND MOVE TO ENVIRONMENT VARIABLES
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from ..settings.settings import app_settings
 
-DOMAIN = "dc.next.dev"
-LDAP_SERVER = f"ldap://{DOMAIN}"
-LDAP_BASE_DN = "DC=NEXT,DC=dev"
+SECRET_KEY: str = app_settings["AUTH"][
+    "secret_key"
+]  # TODO: CHANGE BEFORE DEPLOYMENT AND MOVE TO ENVIRONMENT VARIABLES
+ALGORITHM: str = app_settings["AUTH"]["algorithm"]
+ACCESS_TOKEN_EXPIRE_MINUTES: int = app_settings["AUTH"]["access_token_expire_minutes"]
 
-SCOPES: dict[str, str] = {"student": "student", "teacher": "teacher", "admin": "admin"}
+DOMAIN: str = app_settings["AUTH"]["domain"]
+LDAP_SERVER: str = f"ldap://{DOMAIN}"
+LDAP_BASE_DN: str = app_settings["AUTH"]["ldap_base_dn"]
+
+SCOPES: dict[str, str] = app_settings["AUTH"]["scopes"]
+
+app_settings.save()
