388cf68696f3427db269ac98d95f85ac6137b146
Nicklas H. <gta5.r6s.bf4@gmail.com>
2024-06-17 11:53:52 +0200

    Added documentation to i18n_middleware

--- backend/lib/i18n_middleware.py
+++ backend/lib/i18n_middleware.py
@@ -6,11 +6,35 @@ from typing import Any, Dict
 
 
 class I18nMiddleware(BaseHTTPMiddleware):
+    """
+    Middleware class for handling internationalization (i18n) in HTTP requests.
+
+    This middleware extracts the language from the "Accept-Language" header in the request
+    and sets it in the request state. If the language is not found in the accepted language list,
+    the default language is set to "en-US".
+
+    Attributes:
+        LAN_LIST (list[str]): List of accepted languages.
+
+    Methods:
+        dispatch: Middleware method that sets the language in the request state and calls the next middleware.
+    """
+
     LAN_LIST: list[str] = ["en-US", "en-GB", "da-DK"]
 
     async def dispatch(
         self, request: Request, call_next: RequestResponseEndpoint
     ) -> Response:
+        """
+        Middleware method that sets the language in the request state and calls the next middleware.
+
+        Args:
+            request (Request): The incoming HTTP request.
+            call_next (RequestResponseEndpoint): The next middleware or endpoint to call.
+
+        Returns:
+            Response: The HTTP response returned by the next middleware or endpoint.
+        """
         languages: str = request.headers.get("Accept-Language", default="en-US")
 
         # Accept-Language header example: "en-US,en-GB;q=0.9,en;q=0.8"
@@ -27,9 +51,33 @@ class I18nMiddleware(BaseHTTPMiddleware):
 
 
 class Translator:
+    """
+    Singleton class for translating strings based on language-specific translation files.
+
+    Args:
+        lang (str): The language code for the translator instance.
+
+    Attributes:
+        lang (str): The language code for the translator instance.
+
+    Methods:
+        t(key: str, **kwargs: Dict[str, Any]) -> str:
+            Translates the given key using the language-specific translation files.
+
+    """
+
     _instances: Dict[str, "Translator"] = {}
 
     def __new__(cls, lang: str) -> "Translator":
+        """
+        Create a new instance of the Translator class if it doesn't already exist for the specified language.
+
+        Args:
+            lang (str): The language for which to create the Translator instance.
+
+        Returns:
+            Translator: The existing or newly created Translator instance for the specified language.
+        """
         if lang not in cls._instances:
             cls._instances[lang] = super(Translator, cls).__new__(cls)
         return cls._instances[lang]
@@ -38,6 +86,20 @@ class Translator:
         self.lang: str = lang
 
     def t(self, key: str, **kwargs: Dict[str, Any]) -> str:
+        """
+        Translates the given key to the corresponding string in the specified language.
+
+        Args:
+            key (str): The translation key.
+            **kwargs (Dict[str, Any]): Optional keyword arguments to be used for string formatting.
+
+        Returns:
+            str: The translated string.
+
+        Raises:
+            KeyError: If the translation key is not found in the specified language.
+
+        """
         file_key, *translation_keys = key.split(sep=".")
 
         locale_module: ModuleType = import_module(
@@ -51,4 +113,4 @@ class Translator:
             translation = translation.format(**kwargs)
         if translation is not None:
             return translation
-        return f"Key {key} not found in {self.lang} locale"
+        raise KeyError(f"Key {key} not found in {self.lang} locale")
