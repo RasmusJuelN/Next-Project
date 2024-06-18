21926ad3b024e0a80279e269b4de8ccf905c51a4
Nicklas H. <gta5.r6s.bf4@gmail.com>
2024-06-18 14:19:20 +0200

    Update settings and auth modules to use new dataclass instance

--- backend/lib/auth/constants.py
+++ backend/lib/auth/constants.py
@@ -1,15 +1,13 @@
-from ..settings.settings import app_settings
+from ..settings.settings import app_settings_object
 
-SECRET_KEY: str = app_settings["AUTH"][
-    "secret_key"
-]  # TODO: CHANGE BEFORE DEPLOYMENT AND MOVE TO ENVIRONMENT VARIABLES
-ALGORITHM: str = app_settings["AUTH"]["algorithm"]
-ACCESS_TOKEN_EXPIRE_MINUTES: int = app_settings["AUTH"]["access_token_expire_minutes"]
+SECRET_KEY: str = (
+    app_settings_object.auth.secret_key
+)  # TODO: CHANGE BEFORE DEPLOYMENT AND MOVE TO ENVIRONMENT VARIABLES
+ALGORITHM: str = app_settings_object.auth.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES: int = app_settings_object.auth.access_token_expire_minutes
 
-DOMAIN: str = app_settings["AUTH"]["domain"]
+DOMAIN: str = app_settings_object.auth.domain
 LDAP_SERVER: str = f"ldap://{DOMAIN}"
-LDAP_BASE_DN: str = app_settings["AUTH"]["ldap_base_dn"]
+LDAP_BASE_DN: str = app_settings_object.auth.ldap_base_dn
 
-SCOPES: dict[str, str] = app_settings["AUTH"]["scopes"]
-
-app_settings.save()
+SCOPES: dict[str, str] = app_settings_object.auth.scopes
--- backend/lib/settings/settings.py
+++ backend/lib/settings/settings.py
@@ -1,11 +1,15 @@
 from .settings_manager import SettingsManager
-from .constants import DEFAULT_SETTINGS
+from .models import user_settings, UserSettingsObject
 
 
 app_settings = SettingsManager(
     "./backend-config.yaml",
-    default_settings=DEFAULT_SETTINGS,
+    default_settings=user_settings,
     use_logger=True,
     log_file="./backend/logs/settings.log",
     save_on_exit=True,
+)
+
+app_settings_object: UserSettingsObject = app_settings.to_object(
+    data_class=UserSettingsObject
 )
