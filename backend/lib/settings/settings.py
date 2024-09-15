7eb81f2c30e7e92395eebd13459ca8aa95f031d9
LobaDK <gta5.r6s.bf4@gmail.com>
2024-09-15 21:53:44 +0200

    Fixed settins trying to import non-existent AppSettings instance

--- backend/lib/settings/settings.py
+++ backend/lib/settings/settings.py
@@ -1,11 +1,11 @@
 from backend.lib.settings.settings_manager import SettingsManagerWithDataclass
-from backend.lib.settings.models import AppSettings, app_settings_as_dataclass
+from backend.lib.settings.models import AppSettings
 
 
 app_settings: SettingsManagerWithDataclass[AppSettings] = SettingsManagerWithDataclass[
     AppSettings
 ](
     path="./backend-config.yaml",
-    default_settings=app_settings_as_dataclass,
+    default_settings=AppSettings(),
     autosave=True,
 )
