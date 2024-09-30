bd91df864a2388e4e882582dd35b7655ed2ba878
LobaDK <gta5.r6s.bf4@gmail.com>
2024-09-30 10:43:44 +0200

    Type `selected_option` and `custom_answer` to be optional in the SQL model

--- backend/lib/sql/models.py
+++ backend/lib/sql/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Boolean, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import DeclarativeBase, relationship, mapped_column, Mapped
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 
 class Base(DeclarativeBase):
@@ -30,10 +30,12 @@ class Question(Base):
     )
     title: Mapped[str] = mapped_column(type_=String, index=True)
 
-    selected_option: Mapped[int] = mapped_column(
+    selected_option: Mapped[Optional[int]] = mapped_column(
         type_=Integer, index=False, nullable=True
     )
-    custom_answer: Mapped[str] = mapped_column(type_=String, index=False, nullable=True)
+    custom_answer: Mapped[Optional[str]] = mapped_column(
+        type_=String, index=False, nullable=True
+    )
     template: Mapped["QuestionTemplate"] = relationship(back_populates="questions")
     options: Mapped[List["Option"]] = relationship(
         back_populates="question", cascade="all, delete-orphan"
