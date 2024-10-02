754cbe699a600e20bfb5882f01a75a55c53d7f1e
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-02 14:30:55 +0200

    Add custom `NoLDAPResultsError` and `MissingSaltHashError` exceptions

--- backend/lib/api/auth/exceptions.py
+++ backend/lib/api/auth/exceptions.py
@@ -33,3 +33,36 @@ class MissingADServiceAccountError(Exception):
         super().__init__(
             "An Active Directory service account username and password must be set in the settings file under `auth.ad_service_account` and `auth.ad_service_password`. This account is used to query the LDAP server for user information."
         )
+
+
+class NoLDAPResultsError(Exception):
+    """
+    Exception raised when no results are returned from an LDAP query.
+
+    This exception is raised when an LDAP query returns no results. This could be due to
+    the query being invalid, or the LDAP server not returning any results for the given query.
+
+    Attributes:
+        message (str): Explanation of the error.
+    """
+
+    def __init__(self) -> None:
+        super().__init__("No results were returned from the LDAP query.")
+
+
+class MissingSaltHashError(Exception):
+    """
+    Exception raised when the salt hash is missing in the settings file.
+
+    This exception is used to indicate that the `auth.salt_hash` must be set in the settings file.
+    The salt hash is essential for hashing user data. It should be a long, random string.
+    A suitable key can be generated using the command `openssl rand -hex 32`.
+
+    Attributes:
+        message (str): Explanation of the error.
+    """
+
+    def __init__(self) -> None:
+        super().__init__(
+            "A salt hash in the settings file under `auth.salt_hash` must be set. It is used to hash user data. It should be a long, random string. One can be generated with `openssl rand -hex 32`."
+        )
