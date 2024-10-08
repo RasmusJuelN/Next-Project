ed695949317617764b0a94d8ef1b6c1590f890f3
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-08 10:21:06 +0200

    Turn the auth section of the backend log into a dedicated log file

--- backend/lib/api/auth/__init__.py
+++ backend/lib/api/auth/__init__.py
@@ -5,7 +5,7 @@ from backend.lib._logger import LogHelper
 
 logger: Logger = LogHelper.create_logger(
     logger_name="backend API (auth)",
-    log_file="backend/logs/backend.log",
+    log_file="backend/logs/auth.log",
     file_log_level=DEBUG,
     stream_log_level=INFO,
 )
