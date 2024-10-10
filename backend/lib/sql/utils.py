6ee611d59ad875a49f26aab1f5147d908caf5728
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-10 21:02:11 +0200

    Enhance SQL utility functions with type aliasing and add conditions for user and student/teacher name checks

--- backend/lib/sql/utils.py
+++ backend/lib/sql/utils.py
@@ -1,14 +1,16 @@
 from urllib import parse
-from typing import Dict, Any, Literal, LiteralString
+from typing import Dict, Any, Literal, LiteralString, TypeAlias, Union
 from string import ascii_letters, digits
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, DeclarativeBase
 from sqlalchemy.orm.decl_api import DeclarativeAttributeIntercept
-from sqlalchemy.sql import exists, select
+from sqlalchemy.sql import exists, select, or_, ColumnElement
 
 from backend import app_settings
 from backend.lib.sql import models
 
 URL_FRIENDLY_BASE64: LiteralString = ascii_letters + digits + "-_"
+
+SQLAlchemyModel: TypeAlias = Union[DeclarativeBase, DeclarativeAttributeIntercept]
 
 
 def build_sqlite_connection_args() -> Dict[str, Any]:
@@ -81,11 +83,38 @@ def build_db_connection_args() -> Dict[str, Any]:
         raise ValueError(f"Unsupported database type: {database_type}")
 
 
-def check_if_record_exists_by_id(
-    db: Session, model: DeclarativeAttributeIntercept, id: str
-) -> bool:
+def check_if_record_exists_by_id(db: Session, model: SQLAlchemyModel, id: str) -> bool:
     if hasattr(model, "id"):
         exists_criteria: models.Exists = exists().where(model.id == id)
         return db.execute(statement=select(exists_criteria)).scalar() is True
     else:
         raise ValueError(f"Model {model} does not have an 'id' attribute")
+
+
+def user_id_condition(user_id: str) -> ColumnElement[bool]:
+    return or_(
+        models.ActiveQuestionnaire.student.has(id=user_id),
+        models.ActiveQuestionnaire.teacher.has(id=user_id),
+    )
+
+
+def student_name_condition(student_name: str) -> ColumnElement[bool]:
+    return or_(
+        models.ActiveQuestionnaire.student.has(
+            models.User.user_name.like(other=f"%{student_name}%")
+        ),
+        models.ActiveQuestionnaire.student.has(
+            models.User.full_name.like(other=f"%{student_name}%")
+        ),
+    )
+
+
+def teacher_name_condition(teacher_name: str) -> ColumnElement[bool]:
+    return or_(
+        models.ActiveQuestionnaire.teacher.has(
+            models.User.user_name.like(other=f"%{teacher_name}%")
+        ),
+        models.ActiveQuestionnaire.teacher.has(
+            models.User.full_name.like(other=f"%{teacher_name}%")
+        ),
+    )
