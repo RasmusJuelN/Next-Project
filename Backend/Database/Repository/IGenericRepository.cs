b41c4b81d65701e8465c6822a9ae7d698fb6e575
LobaDK <gta5.r6s.bf4@gmail.com>
2025-02-12 10:43:58 +0100

    Refactor IGenericRepository and SQLGenericRepository to improve method signatures and add patching functionality

--- Backend/Database/Repository/IGenericRepository.cs
+++ Backend/Database/Repository/IGenericRepository.cs
@@ -4,27 +4,31 @@ namespace Database.Repository;
 
 public interface IGenericRepository<TEntity> where TEntity : class
 {
-    public Task<List<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>, IQueryable<TEntity>>? queryModifier = null);
+    Task<List<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>, IQueryable<TEntity>>? queryModifier = null);
     
-    public Task<TEntity?> GetSingleAsync(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>, IQueryable<TEntity>>? queryModifier = null);
+    Task<TEntity?> GetSingleAsync(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>, IQueryable<TEntity>>? queryModifier = null);
 
-    public Task<List<TEntity>> GetAllAsync(Func<IQueryable<TEntity>, IQueryable<TEntity>>? queryModifier = null);
+    Task<List<TEntity>> GetAllAsync(Func<IQueryable<TEntity>, IQueryable<TEntity>>? queryModifier = null);
 
-    public Task<TEntity> AddAsync(TEntity entity);
+    Task<TEntity> AddAsync(TEntity entity);
 
-    public Task<List<TEntity>> AddRangeAsync(List<TEntity> entities);
+    Task<List<TEntity>> AddRangeAsync(List<TEntity> entities);
 
-    public Task<TEntity> UpdateAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity, TEntity existingEntity);
 
-    public Task<TEntity> DeleteAsync(TEntity entity);
-
-    public Task<List<TEntity>> DeleteAsync(Expression<Func<TEntity, bool>> predicate);
-
-    public Task<List<TEntity>> DeleteRangeAsync(List<TEntity> entities);
+    Task<TEntity> PatchAsync(TEntity existingEntity, object newValues);
     
-    public IQueryable<TEntity> GetAsQueryable();
-    
-    public int GetCount(Expression<Func<TEntity, bool>>? predicate = null, Func<IQueryable<TEntity>, IQueryable<TEntity>>? queryModifier = null);
+    Task<List<TEntity>> PatchAsync(Expression<Func<TEntity, bool>> predicate, object newValues);
 
-    public bool Exists(Expression<Func<TEntity, bool>> predicate);
+    Task<TEntity> DeleteAsync(TEntity entity);
+
+    Task<List<TEntity>> DeleteAsync(Expression<Func<TEntity, bool>> predicate);
+
+    Task<List<TEntity>> DeleteRangeAsync(List<TEntity> entities);
+    
+    IQueryable<TEntity> GetAsQueryable();
+    
+    int GetCount(Expression<Func<TEntity, bool>>? predicate = null, Func<IQueryable<TEntity>, IQueryable<TEntity>>? queryModifier = null);
+
+    bool Exists(Expression<Func<TEntity, bool>> predicate);
 }
--- Backend/Database/Repository/SQLGenericRepository.cs
+++ Backend/Database/Repository/SQLGenericRepository.cs
@@ -1,16 +1,25 @@
 using System.Linq.Expressions;
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Database.Repository;
 
-public class SQLGenericRepository<TEntity>(Context context) : IGenericRepository<TEntity> where TEntity : class
+public class SQLGenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
 {
     // TODO: Create custom exceptions and include logging
-    private readonly Context _context = context;
+    private readonly Context _context;
+    private readonly ILogger _logger;
+
+    public SQLGenericRepository(Context context, ILoggerFactory loggerFactory)
+    {
+        _context = context;
+        _logger = loggerFactory.CreateLogger(GetType());
+    }
 
     public async Task<List<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>, IQueryable<TEntity>>? queryModifier = null)
     {
-        IQueryable<TEntity> query = _context.Set<TEntity>().Where(predicate);
+        IQueryable<TEntity> query = GetDbSet().Where(predicate);
 
         if (queryModifier is not null)
         {
@@ -22,7 +31,7 @@ public class SQLGenericRepository<TEntity>(Context context) : IGenericRepository
 
     public async Task<TEntity?> GetSingleAsync(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>, IQueryable<TEntity>>? queryModifier = null)
     {
-        IQueryable<TEntity> query = _context.Set<TEntity>().Where(predicate);
+        IQueryable<TEntity> query = GetDbSet().Where(predicate);
 
         if (queryModifier is not null)
         {
@@ -34,7 +43,7 @@ public class SQLGenericRepository<TEntity>(Context context) : IGenericRepository
 
     public async Task<List<TEntity>> GetAllAsync(Func<IQueryable<TEntity>, IQueryable<TEntity>>? queryModifier = null)
     {
-        IQueryable<TEntity> query = _context.Set<TEntity>();
+        IQueryable<TEntity> query = GetDbSet();
 
         if (queryModifier is not null)
         {
@@ -46,7 +55,7 @@ public class SQLGenericRepository<TEntity>(Context context) : IGenericRepository
 
     public async Task<TEntity> AddAsync(TEntity entity)
     {
-        await _context.Set<TEntity>().AddAsync(entity);
+        await GetDbSet().AddAsync(entity);
         await _context.SaveChangesAsync();
 
         return entity;
@@ -54,23 +63,52 @@ public class SQLGenericRepository<TEntity>(Context context) : IGenericRepository
 
     public async Task<List<TEntity>> AddRangeAsync(List<TEntity> entities)
     {
-        await _context.Set<TEntity>().AddRangeAsync(entities);
+        await GetDbSet().AddRangeAsync(entities);
         await _context.SaveChangesAsync();
 
         return entities;
     }
 
-    public async Task<TEntity> UpdateAsync(TEntity entity)
+    public async Task<TEntity> UpdateAsync(TEntity updatedEntity, TEntity existingEntity)
     {
-        _context.Set<TEntity>().Update(entity);
+        throw new NotImplementedException();
+    }
+
+    public async Task<TEntity> PatchAsync(TEntity existingEntity, object newValues)
+    {
+        if (!ObjectMatchesEntity(newValues))
+        {
+            throw new Exception("Data object does not match entity type.");
+        }
+
+        Patch(existingEntity, newValues);
         await _context.SaveChangesAsync();
 
-        return entity;
+        return existingEntity;
+    }
+
+    public async Task<List<TEntity>> PatchAsync(Expression<Func<TEntity, bool>> predicate, object newValues)
+    {
+        if (!ObjectMatchesEntity(newValues))
+        {
+            throw new Exception("Data object does not match entity type.");
+        }
+
+        List<TEntity> existingEntities = await GetAsync(predicate);
+
+        foreach (TEntity existingEntity in existingEntities)
+        {
+            Patch(existingEntity, newValues);
+        }
+
+        await _context.SaveChangesAsync();
+
+        return existingEntities;
     }
 
     public async Task<TEntity> DeleteAsync(TEntity entity)
     {
-        _context.Set<TEntity>().Remove(entity);
+        GetDbSet().Remove(entity);
         await _context.SaveChangesAsync();
 
         return entity;
@@ -79,7 +117,7 @@ public class SQLGenericRepository<TEntity>(Context context) : IGenericRepository
     public async Task<List<TEntity>> DeleteAsync(Expression<Func<TEntity, bool>> predicate)
     {
         List<TEntity> entities = await GetAsync(predicate);
-        _context.Set<TEntity>().RemoveRange(entities);
+        GetDbSet().RemoveRange(entities);
         await _context.SaveChangesAsync();
 
         return entities;
@@ -87,7 +125,7 @@ public class SQLGenericRepository<TEntity>(Context context) : IGenericRepository
 
     public async Task<List<TEntity>> DeleteRangeAsync(List<TEntity> entities)
     {
-        _context.Set<TEntity>().RemoveRange(entities);
+        GetDbSet().RemoveRange(entities);
         await _context.SaveChangesAsync();
 
         return entities;
@@ -95,12 +133,12 @@ public class SQLGenericRepository<TEntity>(Context context) : IGenericRepository
 
     public IQueryable<TEntity> GetAsQueryable()
     {
-        return _context.Set<TEntity>();
+        return GetDbSet();
     }
 
     public int GetCount(Expression<Func<TEntity, bool>>? predicate = null, Func<IQueryable<TEntity>, IQueryable<TEntity>>? queryModifier = null)
     {
-        IQueryable<TEntity> query = _context.Set<TEntity>();
+        IQueryable<TEntity> query = GetDbSet();
 
         if (predicate != null)
         {
@@ -120,5 +158,63 @@ public class SQLGenericRepository<TEntity>(Context context) : IGenericRepository
         int entityCount = GetCount(predicate);
 
         return entityCount > 0;
+    }
+
+    private DbSet<TEntity> GetDbSet()
+    {
+        return _context.Set<TEntity>();
+    }
+
+    private bool ObjectMatchesEntity(object dataObject)
+    {
+        Type entityType = typeof(TEntity);
+        Type dataObjectType = dataObject.GetType();
+        
+        foreach (PropertyInfo dataProperty in dataObjectType.GetProperties())
+        {
+            PropertyInfo? entityProperty = entityType.GetProperty(dataProperty.Name);
+            
+            if (entityProperty is null)
+            {
+                _logger.LogWarning("Property {dataPropertyName} not found in entity type", dataProperty.Name);
+                return false;
+            }
+
+            if (entityProperty.PropertyType != dataProperty.PropertyType)
+            {
+                _logger.LogWarning("Property {dataPropertyName} with type {dataPropertyDeclaringType} does not match entity property type {entityPropertyClrType}", dataProperty.Name, dataProperty.PropertyType, entityProperty.PropertyType);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Updates the properties of an existing entity with the values from a data object.
+    /// Only non-null properties from the data object will be copied to the existing entity.
+    /// </summary>
+    /// <param name="existingEntity">The entity to be updated.</param>
+    /// <param name="dataObject">The object containing the new values.</param>
+    /// <typeparam name="TEntity">The type of the entity being updated.</typeparam>
+    private static void Patch(TEntity existingEntity, object dataObject)
+    {
+        Type entityType = typeof(TEntity);
+        Type dataObjectType = dataObject.GetType();
+
+        foreach (PropertyInfo dataObjectProperty in dataObjectType.GetProperties())
+        {
+            object? dataObjectValue = dataObjectProperty.GetValue(dataObject);
+
+            if (dataObjectValue is not null)
+            {
+                PropertyInfo? entityProperty = entityType.GetProperty(dataObjectProperty.Name);
+
+                if (entityProperty is not null && entityProperty.CanWrite)
+                {
+                    entityProperty.SetValue(existingEntity, dataObjectValue);
+                }
+            }
+        }
     }
 }
