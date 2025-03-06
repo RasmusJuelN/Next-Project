4d4905e6866e3ec2c1b8860552ca2758e4530def
Nicklas <gta5.r6s.bf4@gmail.com>
2025-03-06 12:51:16 +0100

    Fix query method attribute retrieval in ApplyQueryOrdering extension methods

--- Backend/Database/Extensions/GetQueryMethod.cs
+++ Backend/Database/Extensions/GetQueryMethod.cs
@@ -8,7 +8,7 @@ public static class QueryMethodExtension
 {
     public static IQueryable<T> ApplyQueryOrdering<T>(this TemplateOrderingOptions ordering, IQueryable<T> queryable)
     {
-        QueryMethodAttribute queryMethodAttribute = ordering.GetType().GetCustomAttribute<QueryMethodAttribute>() ?? throw new Exception("QueryMethodAttribute not found");
+        QueryMethodAttribute queryMethodAttribute = ordering.GetType().GetField(ordering.ToString())?.GetCustomAttribute<QueryMethodAttribute>() ?? throw new Exception("QueryMethodAttribute not found");
         string methodName = queryMethodAttribute.QueryMethod;
         MethodInfo methodInfo = typeof(IQueryableExtensions).GetMethod(methodName, BindingFlags.Static | BindingFlags.Public) ?? throw new Exception("Method not found");
         return methodInfo.Invoke(null, [queryable]) as IQueryable<T> ?? throw new Exception("Query method invocation returned null");
@@ -16,7 +16,7 @@ public static class QueryMethodExtension
 
     public static IQueryable<T> ApplyQueryOrdering<T>(this ActiveQuestionnaireOrderingOptions ordering, IQueryable<T> queryable)
     {
-        QueryMethodAttribute queryMethodAttribute = ordering.GetType().GetCustomAttribute<QueryMethodAttribute>() ?? throw new Exception("QueryMethodAttribute not found");
+        QueryMethodAttribute queryMethodAttribute = ordering.GetType().GetField(ordering.ToString())?.GetCustomAttribute<QueryMethodAttribute>() ?? throw new Exception("QueryMethodAttribute not found");
         string methodName = queryMethodAttribute.QueryMethod;
         MethodInfo methodInfo = typeof(IQueryableExtensions).GetMethod(methodName, BindingFlags.Static | BindingFlags.Public) ?? throw new Exception("Method not found");
         return methodInfo.Invoke(null, [queryable]) as IQueryable<T> ?? throw new Exception("Query method invocation returned null");
