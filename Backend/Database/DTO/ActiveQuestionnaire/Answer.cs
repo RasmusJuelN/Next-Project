b78723fba9053006ac46463c30398247e78b92fb
Nicklas <gta5.r6s.bf4@gmail.com>
2025-03-25 08:44:22 +0100

    Change OptionId to nullable in Answer DTO to allow for optional selections

--- Backend/Database/DTO/ActiveQuestionnaire/Answer.cs
+++ Backend/Database/DTO/ActiveQuestionnaire/Answer.cs
@@ -3,6 +3,6 @@ namespace Database.DTO.ActiveQuestionnaire;
 public record class Answer
 {
     public required int QuestionId { get; set; }
-    public required int OptionId { get; set; }
+    public int? OptionId { get; set; }
     public string? CustomAnswer { get; set; }
 }
