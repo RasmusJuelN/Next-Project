139d25518724f69ea94338d1612bfa57676286c4
Nicklas <gta5.r6s.bf4@gmail.com>
2025-03-28 13:08:17 +0100

    Define cascading delete behavior for ActiveQuestionnaire and related entities

--- Backend/Database/Context.cs
+++ Backend/Database/Context.cs
@@ -15,27 +15,66 @@ public class Context : DbContext
         modelBuilder.Entity<QuestionnaireTemplateModel>(e => {
             e.Property(q => q.CreatedAt).HasDefaultValueSql("SYSUTCDATETIME()");
             e.Property(q => q.LastUpated).HasDefaultValueSql("SYSUTCDATETIME()");
+            
+            // Delete Questions linked to a certain questionnaire
+            e.HasMany(q => q.Questions)
+            .WithOne(q => q.QuestionnaireTemplate)
+            .HasPrincipalKey(q => q.Id)
+            .HasForeignKey(q => q.QuestionnaireTemplateFK)
+            .OnDelete(DeleteBehavior.Cascade);
+            
+            // Delete active questionnaires that reference the deleted questionnaire
+            e.HasMany(q => q.ActiveQuestionnaires)
+            .WithOne(a => a.QuestionnaireTemplate)
+            .HasPrincipalKey(q => q.Id)
+            .HasForeignKey(a => a.QuestionnaireTemplateFK)
+            .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        // QuestionnaireTemplateQuestion
+        modelBuilder.Entity<QuestionnaireQuestionModel>(e => {
+            e.HasMany(q => q.Options)
+            .WithOne(o => o.Question)
+            .HasPrincipalKey(q => q.Id)
+            .HasForeignKey(o => o.QuestionFK)
+            .OnDelete(DeleteBehavior.Cascade);
         });
         
         // ActiveQuestionnaireModel
         modelBuilder.Entity<ActiveQuestionnaireModel>(e => {
             e.Property(a => a.ActivatedAt)
             .HasDefaultValueSql("SYSUTCDATETIME()");
+            
+            // We don't wanna delete users since they might have other active questionnaires assigned
             e.HasOne(a => a.Student)
             .WithMany(u => u.ActiveQuestionnaires)
+            .HasPrincipalKey(u => u.Id)
+            .HasForeignKey(a => a.StudentFK)
             .OnDelete(DeleteBehavior.NoAction);
             e.HasOne(a => a.Teacher)
             .WithMany(u => u.ActiveQuestionnaires)
+            .HasPrincipalKey(u => u.Id)
+            .HasForeignKey(a => a.TeacherFK)
             .OnDelete(DeleteBehavior.NoAction);
+            
+            // Deleting an active questionnaire shouldn't delete the template
             e.HasOne(a => a.QuestionnaireTemplate)
             .WithMany(t => t.ActiveQuestionnaires)
+            .HasPrincipalKey(t => t.Id)
+            .HasForeignKey(a => a.QuestionnaireTemplateFK)
             .OnDelete(DeleteBehavior.NoAction);
+            
+            // No reason to keep the answers if we're deleting the active questionnaire
             e.HasMany(a => a.StudentAnswers)
             .WithOne(r => r.ActiveQuestionnaire)
-            .OnDelete(DeleteBehavior.NoAction);
+            .HasPrincipalKey(a => a.Id)
+            .HasForeignKey(r => r.ActiveQuestionnaireFK)
+            .OnDelete(DeleteBehavior.Cascade);
             e.HasMany(a => a.TeacherAnswers)
             .WithOne(r => r.ActiveQuestionnaire)
-            .OnDelete(DeleteBehavior.NoAction);
+            .HasPrincipalKey(a => a.Id)
+            .HasForeignKey(r => r.ActiveQuestionnaireFK)
+            .OnDelete(DeleteBehavior.Cascade);
         });
 
         // UserModel
---
+++ Backend/Database/Migrations/20250328115946_Explicitly define the key relationships and delete behavior.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Explicitlydefinethekeyrelationshipsanddeletebehavior : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ActiveQuestionnaireResponse_ActiveQuestionnaire_ActiveQuestionnaireFK",
+                table: "ActiveQuestionnaireResponse");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ActiveQuestionnaireResponse_ActiveQuestionnaire_ActiveQuestionnaireFK",
+                table: "ActiveQuestionnaireResponse",
+                column: "ActiveQuestionnaireFK",
+                principalTable: "ActiveQuestionnaire",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ActiveQuestionnaireResponse_ActiveQuestionnaire_ActiveQuestionnaireFK",
+                table: "ActiveQuestionnaireResponse");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ActiveQuestionnaireResponse_ActiveQuestionnaire_ActiveQuestionnaireFK",
+                table: "ActiveQuestionnaireResponse",
+                column: "ActiveQuestionnaireFK",
+                principalTable: "ActiveQuestionnaire",
+                principalColumn: "Id");
+        }
+    }
+}
