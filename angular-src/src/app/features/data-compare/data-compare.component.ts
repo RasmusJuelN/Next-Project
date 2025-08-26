d4b4863d40a6580f04cbdb54dcc3c12bf662f1b4
NickDieni <nickevereye@gmail.com>
2025-08-26 08:44:16 +0200

    The start of something great, a new page called data-compare, and added to header and routing

---
+++ angular-src/src/app/features/data-compare/data-compare.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-compare',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-compare.component.html',
+  styleUrl: './data-compare.component.css'
+})
+export class DataCompareComponent {
+
+}
