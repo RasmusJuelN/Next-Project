2439b80db98f9b06709d42a72b8edde905da4102
Nicklas <gta5.r6s.bf4@gmail.com>
2025-10-11 12:08:08 +0200

    Enhance LDAP authentication by adding SSL support and updating connection options

--- Backend/Settings/Default/DefaultLDAP.cs
+++ Backend/Settings/Default/DefaultLDAP.cs
@@ -6,6 +6,8 @@ public class DefaultLDAP : ILDAPSettings
 {
     public string Host { get; set; } = "example.com";
     public int Port { get; set; } = 389;
+    public int SSLPort { get; set; } = 636;
+    public bool UseSSL { get; set; } = false;
     public string FQDN { get; set; } = "ad.example.com";
     public string BaseDN { get; set; } = "DC=example,DC=com";
     public string SA { get; set; } = string.Empty;
