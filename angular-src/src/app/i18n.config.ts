fba4be206f1ffd45b4c2c08ff2f1f36b2caa8865
Max Felding <maxjac64@gmail.com>
2025-10-28 12:36:59 +0100

    updated langue settings

---
+++ angular-src/src/app/i18n.config.ts
@@ -0,0 +1,29 @@
+export enum Lang {
+  EN = 'en',
+  DA = 'da',
+}
+
+export interface I18nLanguageConfig {
+  code: Lang;
+  label: string;
+  flagSrc: string;
+  default?: boolean;
+}
+
+export const DEFAULT_LANGUAGE: Lang = Lang.DA;
+
+
+export const SUPPORTED_LANGUAGES: I18nLanguageConfig[] = [
+  {
+    code: Lang.EN,
+    label: 'LANG.EN',
+    flagSrc: 'assets/icons/united-states-flag-icon.svg',
+  },
+  {
+    code: Lang.DA,
+    label: 'LANG.DA',
+    flagSrc: 'assets/icons/denmark-flag-icon.svg',
+    default: true,
+  },
+];
+export const SUPPORTED_CODES = SUPPORTED_LANGUAGES.map(l => l.code) as string[];
